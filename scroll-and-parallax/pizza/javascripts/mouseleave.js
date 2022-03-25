eb2bd937f116ba6eb9ed571c7388405f7b4b3b63
luiquecruz <luiquecruz@gmail.com>
2022-03-25 00:08:06 -0300

    Initial commit

---
+++ scroll-and-parallax/pizza/javascripts/mouseleave.js
@@ -0,0 +1,28 @@
+$(function () {
+  $(window).mouseleave(function (e) {
+    if (e.toElement == null) {
+      document.documentElement.classList.add('mouse-out');
+    }
+  });
+
+  $('#close-modal').click(function (e) {
+    e.preventDefault();
+    document.documentElement.classList.remove('mouse-out');
+  });
+});
+
+// with local storage
+// $(function () {
+//   $(window).mouseleave(function (e) {
+//     var modalSeen = sessionStorage.getItem('modalSeen');
+//     if (e.toElement == null && !modalSeen) {
+//       document.documentElement.classList.add('mouse-out');
+//     }
+//   });
+
+//   $('#close-modal').click(function (e) {
+//     e.preventDefault();
+//     document.documentElement.classList.remove('mouse-out');
+//     sessionStorage.setItem('modalSeen', true);
+//   });
+// });
